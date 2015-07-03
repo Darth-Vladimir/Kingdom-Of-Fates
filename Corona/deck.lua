f7324d4e21b0eabf1b94c277d1fd48a8d1e4d093
Darth-Vladimir <stefan.vanstaden5@gmail.com>
2015-07-03 14:13:45 +0200

    Updated deck.lua
    Added functionality to deck.lua

--- Corona/deck.lua
+++ Corona/deck.lua
@@ -1,14 +1,60 @@
-local cards = {}
+local deck = {}
+local PlayDeck ={} -- clone of deck 
 local deckName
-local deckSize = 30
-local composer = require( "card" )
+local deckSize = 40
+local card = require( "card" )
 function deck.setName(Name)
 	deckName=Name
+end
+--Clone deck call this function at the start of the game
+function deck.Clone()
+	PlayDeck = table.copy(deck)
+end
+--Shuffle PlayDeck
+function deck.Shuffle()
+	math.randomseed( os.time() )
+    local rand = math.random 
+    assert( PlayDeck, "shuffleTable() expected a table, got nil" )
+    local iterations = #PlayDeck
+    local j
+    
+    for i = iterations, 2, -1 do
+        j = rand(i)
+        PlayDeck[i], PlayDeck[j] = PlayDeck[j], PlayDeck[i]
+    end
+end
+--Draws the top card from the PlayDeck
+function deck.Draw()
+	local CurCard = unpack(PlayDeck,#PlayDeck)
+	table.remove(PlayDeck,#PlayDeck)
+	return CurCard
+end
+
+--Shuffle card into PlayDeck
+function deck.Insert(SomeCard)
+	table.insert(PlayDeck,#PlayDeck,SomeCard)
+	deck.Shuffle()
+end
+--Get card a specific position
+function deck.GetCard(position)
+	if position<=#PlayDeck then
+		local CurCard = unpack(PlayDeck,position)
+		return CurCard
+	end
+end
+--Remove card at position
+function deck.removeCard(position)
+	if position<=#PlayDeck then
+		local CurCard = unpack(PlayDeck,position)
+		table.remove(PlayDeck,position)
+	end
 end
 --Starter Deck
 --loads a default starter deck from server into cards
 for i = 1,deckSize do
 	--Name, Description, Element, Type, Image, Effects, Attack, Defence, Level, Parent is retreived from server
-	local card = card.new(Name, Description, Element, Type, Image, Effects, Attack, Defence, Level, Parent)
-	table.insert(cards,#cards,card)
+	local CurCard = card.new(Name, Description, Element, Type, Image, Effects, Attack, Defence, Level, Parent)
+	table.insert(deck,#deck,CurCard)
 end
+
+return deck
--- Corona/player.lua
+++ Corona/player.lua
@@ -1,8 +1,13 @@
-local player = {}
+local card = require("card")
 
+local player = {}
+local cards = {}
 local lifepoints
 local field
 local hand
 local deck
-
+--Connects to server and loads cards
+function player.LoadCards(){
+	--To Complete
+}
 return player
